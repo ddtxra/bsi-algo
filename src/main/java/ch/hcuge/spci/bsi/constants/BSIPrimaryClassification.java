1a43ac68901ce63f9d9dd65b7c6e427b626a2dd1
HUGE\ddii <Daniel.DinisTeixeira@hcuge.ch>
2023-04-21 18:00:30 +0200

    adding build yaml file

---
+++ src/main/java/ch/hcuge/spci/bsi/constants/BSIPrimaryClassification.java
@@ -0,0 +1,11 @@
+package ch.hcuge.spci.bsi.constants;
+
+public enum BSIPrimaryClassification {
+
+    PRIMARY_ASSOCIATED, //CA-BS
+    PRIMARY_RELATED, //CR-BSI
+
+    //géneralement pas de cath présent
+    PRIMARY_OTHER
+
+}
